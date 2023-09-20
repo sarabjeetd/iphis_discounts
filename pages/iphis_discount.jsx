af5b5b7c8bfa2890651fc5c8056472d4d870ec00
Sarabjeet Dhillon <sdhillon.zs@gmail.com>
2023-09-20 13:56:22 -0600

    jsx changes

--- pages/iphis_discount.jsx
+++ pages/iphis_discount.jsx
@@ -1,8 +1,10 @@
+import React, { useState } from 'react';
 import useFetch from "@/components/hooks/useFetch";
 import { Page, Layout, LegacyCard } from "@shopify/polaris";
 
 const IphisDiscountPage = () => {
     const fetch = useFetch();
+    const [content, setContent] = useState('');
     return (
         <>
             <Page>
@@ -13,10 +15,16 @@ const IphisDiscountPage = () => {
                                    onAction:async()=> {
                                     const response = await (await fetch("/api/apps")).json();
                                     console.log(response);
+                                    if(response.body.data.discountAutomaticAppCreate.userErrors.length > 0){
+                                        setContent("Error: Please contact the app developer");
+                                    }
+                                    else{
+                                        setContent("Success: Discount is now installed. Please go to Discounts to see new discount");
+                                    }
                                    } 
                                 }}
                             >
-                            <p></p>
+                            <p>{content}</p>
                         </LegacyCard>
                     </Layout.Section>
                 </Layout>
